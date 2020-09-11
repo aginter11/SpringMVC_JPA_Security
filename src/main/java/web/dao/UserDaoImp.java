c1cc55ceccc41d0d1fe9c1cb8efb409ac5303985
Artur <aginter11@ya.ru>
2020-09-11 13:05:55 +0500

    commit

--- src/main/java/web/dao/UserDaoImp.java
+++ src/main/java/web/dao/UserDaoImp.java
@@ -1,15 +1,11 @@
 package web.dao;
 
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import web.model.User;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
@@ -21,23 +17,19 @@ public class UserDaoImp implements UserDao {
 
     @Override
     public void add(User user) {
-        //     sessionFactory.getCurrentSession().save(user);
+
         entityManager.persist(user);
     }
 
     @Override
     public void updateUser(User user) {
-        //   sessionFactory.getCurrentSession().update(user);
+
         entityManager.merge(user);
     }
 
     @Override
     public void removeUser(int id) {
-//        Session session = sessionFactory.getCurrentSession();
-//        User user = (User) session.get(User.class, id);
-//        if (user != null) {
-//            session.delete(user);
-//        }
+
         User user = entityManager.find(User.class, id);
         entityManager.remove(user);
 
@@ -45,8 +37,6 @@ public class UserDaoImp implements UserDao {
 
     @Override
     public User getUserbyId(int id) {
-//        Session session = sessionFactory.getCurrentSession();
-//        User user = (User) session.get(User.class, id);
 
         User user = entityManager.find(User.class, id);
         return user;
@@ -55,8 +45,6 @@ public class UserDaoImp implements UserDao {
     @Override
     @SuppressWarnings("unchecked")
     public List<User> listUsers() {
-//        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
-//        return query.getResultList();
 
         return entityManager.createQuery("SELECT u from User u").getResultList();
     }
